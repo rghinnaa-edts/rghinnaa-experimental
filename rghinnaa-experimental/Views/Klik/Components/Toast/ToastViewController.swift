eb8196479983055e97a6b53ee0bbd0b09bc2adfe
yovitaHandayiani <116541773+yovitaHandayiani@users.noreply.github.com>
2026-03-04 09:32:58 +0700

    fix: ToastViewController

--- rghinnaa-experimental/Views/Klik/Components/Toast/ToastViewController.swift
+++ rghinnaa-experimental/Views/Klik/Components/Toast/ToastViewController.swift
@@ -9,22 +9,14 @@ import UIKit
 import KlikIDM_DS
 
 class ToastViewController: UIViewController {
-    
     @IBAction func testButton(_ sender: Any) {
         ToastManager.toast.show(
             rootView: self.view,
-            duration: .indefinite,
-            animation: .slide,
-            swipeDirection: .vertical,
             configure: { toast in
                 toast.toastState = "info"
                 toast.iconLeading = UIImage(named: "placeholder")
                 toast.configureButton{ btn in
                     btn.label = "Button"
-                    btn.iconTrailing = UIImage(systemName: "chevron.right")
-                }
-                toast.configureIconButton{ btn in
-                    btn.icon = UIImage(systemName: "chevron.right")
                 }
             })
     }
