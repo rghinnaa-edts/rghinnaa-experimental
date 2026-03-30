0dddbe80e527ac3d32afa3511e1fb8988067312f
Rizka Ghinna Auliya <rizka.ghinna@sg-edts.com>
2026-03-30 11:18:37 +0700

    feat: coachmark

--- rghinnaa-experimental/Views/Klik/Components/Coachmark/CoachmarkKlikViewController.swift
+++ rghinnaa-experimental/Views/Klik/Components/Coachmark/CoachmarkKlikViewController.swift
@@ -5,3 +5,44 @@
 //  Created by Rizka Ghinna Auliya on 06/03/26.
 //
 
+import UIKit
+import KlikIDM_DS
+
+class CoachmarkKlikViewController: UIViewController {
+    
+    @IBOutlet weak var vCard1: UIView!
+    @IBOutlet weak var vCard2: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        DispatchQueue.main.async {
+            self.showCoachmark()
+        }
+        
+    }
+    
+    func showCoachmark() {
+        let coachmark = CoachmarkKlik(frame: .zero)
+
+        coachmark.stepConjunction = "dari"
+        coachmark.btnFirstTint = UIColor.red
+        coachmark.btnSecondTint = UIColor.purple
+        
+        coachmark.configureSteps(steps: [
+            CoachmarkStepConfig(
+                title: "Step 1",
+                description: "The quick brown fox jumps over the lazy dog",
+                targetView: vCard1
+            ),
+            
+            CoachmarkStepConfig(
+                title: "Step 2",
+                description: "This step shows both start and end targets with dual spotlights",
+                targetView: vCard2
+            )
+        ])
+
+        coachmark.show()
+    }
+}
