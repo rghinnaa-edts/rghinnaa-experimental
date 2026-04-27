61495d89c3f699335d65e41d63ab7471413d0909
yovitaHandayiani <116541773+yovitaHandayiani@users.noreply.github.com>
2026-04-27 15:57:35 +0700

    feat: radio button

--- rghinnaa-experimental/Views/Klik/Components/RadioButton/RadioButtonViewController.swift
+++ rghinnaa-experimental/Views/Klik/Components/RadioButton/RadioButtonViewController.swift
@@ -9,7 +9,18 @@ import UIKit
 import KlikIDM_DS
 
 class RadioButtonViewController: UIViewController{
+    
+    @IBOutlet weak var radioGroup: RadioGroup!
+    @IBOutlet weak var radioBtn: RadioButton!
+    
+    var display: [[String]] = [["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"], ["Title radio button", "Body text goes here"]]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        radioGroup.data = display
+        radioGroup.displayMode = .vertical
+        radioGroup.selectedIndex = 0
+        radioGroup.paddingLeading = 8
     }
 }
